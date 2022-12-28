442f13c9182713d25bb256165f05166b47c2a9df
a.stan <a.stan@kaizengaming.com>
2022-12-28 14:28:47 +0200

    Typo

--- src/helpers/config_helpers.py
+++ src/helpers/config_helpers.py
@@ -1,7 +1,7 @@
 import os
 
 def get_base_url():
-    env = os.environ.get('ENV', 'prod')
+    env = os.environ.get('ENV', 'test')
 
     if env.lower() == 'test':
         return 'http://localhost:8888/coolsite/'
