f3b9ad8783c1552c9858078f6f7b931c4cd3ed7f
a.stan <a.stan@kaizengaming.com>
2022-12-28 13:38:21 +0200

    Added database_helpers.py

---
+++ src/helpers/database_helpers.py
@@ -0,0 +1,26 @@
+import pymysql
+
+def read_from_db(sql):
+    # connect to db
+    connection = pymysql.connect(host='127.0.0.1', port=8889,
+                                user='root', password='root')
+
+    # read from db
+    try: 
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        db_data = cursor.fetchall()
+        cursor.close()
+        connection.close()
+    finally:
+        connection.close()
+
+    # return the result
+    return db_data
+
+def get_order_from_db_by_order_nr(order_nr):
+    sql = f"SELECT * FROM coolsite.wp_posts WHERE ID = {order_nr} AND post_type = 'shop_order';"
+    db_order = read_from_db(sql)
+    print(db_order)
+
+get_order_from_db_by_order_nr(65)
