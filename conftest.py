5aed9558cc15c6cb2e0d7634bd09eb723e91526d
a.stan <a.stan@kaizengaming.com>
2022-12-29 13:48:03 +0200

    Added tuning for report generation.

--- conftest.py
+++ conftest.py
@@ -10,4 +10,18 @@ def init_driver(request):
     yield
     driver.quit()
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            is_frontend_test = True if 'init_driver' in item.fixturenames else False
+            if is_frontend_test:
+                extra.append(pytest_html.extras.url("http://www.example.com/"))
+                
+        report.extra = extra
 
